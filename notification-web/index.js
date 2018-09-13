895fbca3a602c5a2e19bcd917163be5bb9201324
truexinology <truexin@163.com>
2018-09-13 17:37:28 +0800

    [update] 更新websocket和浏览器notification-web

---
+++ notification-web/index.js
@@ -0,0 +1,26 @@
+let express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendfile('index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('notification', function (msg) {
+        console.log(msg);
+        io.emit('notification', msg);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
+
+http.listen(8081, function () {
+    console.log('listening on port 8081');
+})
